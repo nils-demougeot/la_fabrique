d9aca37ed5056170dc586529ae538d8a561eaa45
Nils <72404130+nils-demougeot@users.noreply.github.com>
2026-04-10 23:32:36 +0200

    changement pour connexion database postgres en ligne

--- lafabrique/settings.py
+++ lafabrique/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +79,18 @@ WSGI_APPLICATION = 'lafabrique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# Configuration locale par défaut (SQLite)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Si la variable DATABASE_URL existe (ce qui sera le cas sur Render), on écrase la configuration
+database_url = os.environ.get("DATABASE_URL")
+if database_url:
+    DATABASES['default'] = dj_database_url.config(default=database_url, conn_max_age=600)
 
 
 # Password validation
