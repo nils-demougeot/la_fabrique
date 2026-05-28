886eb64819a89b28703b61e6a3c77325c65663eb
Nils <72404130+nils-demougeot@users.noreply.github.com>
2026-05-28 16:03:17 +0200

    mdoif admin V3

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Utilisateur, Vetement, Patron, Tutoriel, Projet, Recommandation, AchatPatron
+from .models import Utilisateur, Vetement, Patron, EtapePatron, Tutoriel, Projet, Recommandation, AchatPatron
+
+
+class EtapePatronInline(admin.StackedInline):
+    model = EtapePatron
+    extra = 0
+    ordering = ['numero']
+    fields = ['numero', 'titre', 'description', 'conseil', 'materiaux_etape', 'video_url']
 
 
 class TutorielInline(admin.TabularInline):
@@ -14,7 +21,7 @@ class PatronAdmin(admin.ModelAdmin):
     list_display  = ['titre', 'typeObjet', 'difficulte', 'duree', 'surfaceMin', 'surfaceMax', 'estPremium']
     list_filter   = ['estPremium', 'difficulte', 'typeObjet']
     search_fields = ['titre', 'description']
-    inlines       = [TutorielInline]
+    inlines       = [EtapePatronInline, TutorielInline]
     fieldsets = [
         (None, {
             'fields': ['titre', 'photo', 'description', 'typeObjet', 'materiel'],
