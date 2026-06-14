7926bf87bc1f9e8cd3e056aaf8ea88674571dcdd
Nils <72404130+nils-demougeot@users.noreply.github.com>
2026-06-15 01:08:30 +0200

    debug

--- lafabrique/settings.py
+++ lafabrique/settings.py
@@ -167,3 +167,8 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'home' 
 
 LOGIN_URL = 'connexion'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
